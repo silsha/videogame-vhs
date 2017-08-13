c3e0a39e32835a173e0b02bf13e997b6f3518ac4
silsha <hallo@silsha.me>
2017-08-13 12:29:15 +0200

    Remove old template

--- themes/gamevideos/index.js
+++
@@ -1,6 +0,0 @@
-var algoliasearch = require('algoliasearch');
-
-var client = algoliasearch("9HOA31XFL6", "0e1be579f8af57d9919139301f0c1b25");
-var index = client.initIndex('videos');
-
-console.log(index);
